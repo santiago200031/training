f773ed4484c5ec66eac53b08da4eb1f716bd3b3a
Santiago Villavicencio <66896776+santiago200031@users.noreply.github.com>
2021-04-29 01:14:42 +0200

    Correctura

---
+++ Spring/src/main/java/Prueba001/App.java
@@ -0,0 +1,10 @@
+package Prueba001;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Spring/src/main/java/Prueba001/Persona.java
@@ -0,0 +1,8 @@
+package Prueba001;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Persona {
+
+}
