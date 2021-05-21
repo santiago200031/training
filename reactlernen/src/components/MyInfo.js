b9c22c963e946484c35f7a91767614512bfb6013
Santiago Villavicencio <66896776+santiago200031@users.noreply.github.com>
2021-05-21 22:55:21 +0200

    Component Creation

---
+++ reactlernen/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Santiago Villavicencio</h1>
+            <p>Ich bin Santiago Villavicencio und lerne zurzeit React</p>
+            <ol>
+                <li id="3">Würzburg</li>
+                <li id="2">Russland</li>
+                <li id="1">Augsburg</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyInfo
