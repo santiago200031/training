5173764e2ea2ea1679a0868b5f63bd400525e417
Santiago <villavicencioandrs@gmail.com>
2024-02-03 14:28:50 +0100

    Added Finance as return Typ for JSON conversion

--- chart-finance-tracker/src/main/java/org/deeplearning/interfaces/AIControl.java
+++ chart-finance-tracker/src/main/java/org/deeplearning/interfaces/AIControl.java
@@ -1,13 +1,14 @@
 package org.deeplearning.interfaces;
 
 import org.deeplearning.plots.PlotFinance;
+import org.finance.models.Finance;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 
 public interface AIControl {
 
     DataSetIterator loadCsvData(String filePath);
 
-    String makePrediction(String dateString);
+    Finance makePrediction(String dateString);
 
     PlotFinance visualizeData();
 
